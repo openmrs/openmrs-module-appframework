03f4240841157d63d56149b99e09609989bff214
wluyima <wyclif@openmrs.org>
2013-04-08 15:23:50 -0400

    Applying auto format changes

--- api/src/main/java/org/openmrs/module/appframework/repository/AllExtensions.java
+++ api/src/main/java/org/openmrs/module/appframework/repository/AllExtensions.java
@@ -1,57 +1,60 @@
 package org.openmrs.module.appframework.repository;
 
-import org.openmrs.module.appframework.domain.Extension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-
-import javax.validation.ConstraintViolation;
-import javax.validation.Validator;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+
+import org.openmrs.module.appframework.domain.Extension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 @Repository
 public class AllExtensions {
-
-    private List<Extension> extensions = new ArrayList<Extension>();
-
-    private Validator validator;
-
-    @Autowired
-    public AllExtensions(Validator validator) {
-        this.validator = validator;
-    }
-
-    public void add(List<Extension> extensions) {
-        for (Extension extension : extensions) {
-            add(extension);
-        }
-    }
-
-    public void add(Extension extension) {
-        validate(extension);
-
-        synchronized (this.extensions) {
-            this.extensions.add(extension);
-            Collections.sort(this.extensions);
-        }
-    }
-
-    private void validate(Extension extension) {
-        Set<ConstraintViolation<Extension>> constraintViolations = validator.validate(extension);
-        if (!constraintViolations.isEmpty()) throw new IllegalArgumentException(constraintViolations.iterator().next().getMessage());
-
-        if (this.extensions.contains(extension)) throw new IllegalArgumentException("Extension already exists");
-    }
-
-    public List<Extension> getExtensions() {
-        List<Extension> extensionList = new ArrayList<Extension>();
-        extensionList.addAll(this.extensions);
-        return extensionList;
-    }
-
-    public void clear() {
-        extensions.clear();
-    }
+	
+	private List<Extension> extensions = new ArrayList<Extension>();
+	
+	private Validator validator;
+	
+	@Autowired
+	public AllExtensions(Validator validator) {
+		this.validator = validator;
+	}
+	
+	public void add(List<Extension> extensions) {
+		for (Extension extension : extensions) {
+			add(extension);
+		}
+	}
+	
+	public void add(Extension extension) {
+		validate(extension);
+		
+		synchronized (this.extensions) {
+			this.extensions.add(extension);
+			Collections.sort(this.extensions);
+		}
+	}
+	
+	private void validate(Extension extension) {
+		Set<ConstraintViolation<Extension>> constraintViolations = validator.validate(extension);
+		if (!constraintViolations.isEmpty())
+			throw new IllegalArgumentException(constraintViolations.iterator().next().getMessage());
+		
+		if (this.extensions.contains(extension))
+			throw new IllegalArgumentException("Extension already exists");
+	}
+	
+	public List<Extension> getExtensions() {
+		List<Extension> extensionList = new ArrayList<Extension>();
+		extensionList.addAll(this.extensions);
+		return extensionList;
+	}
+	
+	public void clear() {
+		extensions.clear();
+	}
 }
