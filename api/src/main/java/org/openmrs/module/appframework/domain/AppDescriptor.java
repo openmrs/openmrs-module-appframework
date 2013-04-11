e5d625d02a9dcde25b68a4c8a4e5f56b513abc85
wluyima <wyclif@openmrs.org>
2013-04-11 11:29:05 -0400

    Applying auto format changes

--- api/src/main/java/org/openmrs/module/appframework/domain/AppDescriptor.java
+++ api/src/main/java/org/openmrs/module/appframework/domain/AppDescriptor.java
@@ -1,135 +1,138 @@
 package org.openmrs.module.appframework.domain;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.codehaus.jackson.annotate.JsonProperty;
 import org.hibernate.validator.constraints.NotEmpty;
 import org.openmrs.module.appframework.domain.validators.NoDuplicateExtensionPoint;
 import org.openmrs.module.appframework.domain.validators.ValidationErrorMessages;
 
-import javax.validation.Valid;
-import java.util.List;
-
 @NoDuplicateExtensionPoint
 public class AppDescriptor implements Comparable<AppDescriptor> {
-
-    @NotEmpty(message = ValidationErrorMessages.APP_DESCRIPTOR_ID_NOT_EMPTY_MESSAGE)
-    @JsonProperty
-    protected String id;
-
-    @JsonProperty
-    protected String description;
-
-    @JsonProperty
-    protected String label;
-
-    @JsonProperty
-    protected String url;
-
-    @JsonProperty
-    protected String iconUrl;
-
-    @JsonProperty
-    protected String tinyIconUrl;
-
-    @JsonProperty
-    protected int order;
-
-    @Valid
-    @JsonProperty
-    protected List<ExtensionPoint> extensionPoints;
-
-    @JsonProperty
-    protected List<String> contextModel;
-
-
-    public AppDescriptor() {
-    }
-
-    public AppDescriptor(String id, String description, String label, String url, String iconUrl, String tinyIconUrl, int order) {
-        this.id = id;
-        this.description = description;
-        this.label = label;
-        this.url = url;
-        this.iconUrl = iconUrl;
-        this.tinyIconUrl = tinyIconUrl;
-        this.order = order;
-    }
-
-    public AppDescriptor(String id, String description, String label, String url, String iconUrl, String tinyIconUrl,
-                         int order, List<ExtensionPoint> extensionPoints) {
-        this.id = id;
-        this.description = description;
-        this.label = label;
-        this.url = url;
-        this.iconUrl = iconUrl;
-        this.tinyIconUrl = tinyIconUrl;
-        this.order = order;
-        this.extensionPoints = extensionPoints;
-    }
-
-
-    public String getId() {
-        return id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public String getLabel() {
-        return label;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public String getIconUrl() {
-        return iconUrl;
-    }
-
-    public String getTinyIconUrl() {
-        return tinyIconUrl;
-    }
-
-    public int getOrder() {
-        return order;
-    }
-
-    public List<ExtensionPoint> getExtensionPoints() {
-        return extensionPoints;
-    }
-
-    public List<String> getContextModel() {
-        return contextModel;
-    }
-
-    public void setExtensionPoints(List<ExtensionPoint> extensionPoints) {
-        this.extensionPoints = extensionPoints;
-    }
-
-    public void setContextModel(List<String> contextModel) {
-        this.contextModel = contextModel;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof AppDescriptor)) return false;
-
-        AppDescriptor that = (AppDescriptor) o;
-
-        if (id != null ? !id.equals(that.id) : that.id != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        return id != null ? id.hashCode() : 0;
-    }
-
-    @Override
-    public int compareTo(AppDescriptor o) {
-        return new Integer(this.order).compareTo(new Integer(o.order));
-    }
+	
+	@NotEmpty(message = ValidationErrorMessages.APP_DESCRIPTOR_ID_NOT_EMPTY_MESSAGE)
+	@JsonProperty
+	protected String id;
+	
+	@JsonProperty
+	protected String description;
+	
+	@JsonProperty
+	protected String label;
+	
+	@JsonProperty
+	protected String url;
+	
+	@JsonProperty
+	protected String iconUrl;
+	
+	@JsonProperty
+	protected String tinyIconUrl;
+	
+	@JsonProperty
+	protected int order;
+	
+	@Valid
+	@JsonProperty
+	protected List<ExtensionPoint> extensionPoints;
+	
+	@JsonProperty
+	protected List<String> contextModel;
+	
+	public AppDescriptor() {
+	}
+	
+	public AppDescriptor(String id, String description, String label, String url, String iconUrl, String tinyIconUrl,
+	    int order) {
+		this.id = id;
+		this.description = description;
+		this.label = label;
+		this.url = url;
+		this.iconUrl = iconUrl;
+		this.tinyIconUrl = tinyIconUrl;
+		this.order = order;
+	}
+	
+	public AppDescriptor(String id, String description, String label, String url, String iconUrl, String tinyIconUrl,
+	    int order, List<ExtensionPoint> extensionPoints) {
+		this.id = id;
+		this.description = description;
+		this.label = label;
+		this.url = url;
+		this.iconUrl = iconUrl;
+		this.tinyIconUrl = tinyIconUrl;
+		this.order = order;
+		this.extensionPoints = extensionPoints;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public String getLabel() {
+		return label;
+	}
+	
+	public String getUrl() {
+		return url;
+	}
+	
+	public String getIconUrl() {
+		return iconUrl;
+	}
+	
+	public String getTinyIconUrl() {
+		return tinyIconUrl;
+	}
+	
+	public int getOrder() {
+		return order;
+	}
+	
+	public List<ExtensionPoint> getExtensionPoints() {
+		return extensionPoints;
+	}
+	
+	public List<String> getContextModel() {
+		return contextModel;
+	}
+	
+	public void setExtensionPoints(List<ExtensionPoint> extensionPoints) {
+		this.extensionPoints = extensionPoints;
+	}
+	
+	public void setContextModel(List<String> contextModel) {
+		this.contextModel = contextModel;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (!(o instanceof AppDescriptor))
+			return false;
+		
+		AppDescriptor that = (AppDescriptor) o;
+		
+		if (id != null ? !id.equals(that.id) : that.id != null)
+			return false;
+		
+		return true;
+	}
+	
+	@Override
+	public int hashCode() {
+		return id != null ? id.hashCode() : 0;
+	}
+	
+	@Override
+	public int compareTo(AppDescriptor o) {
+		return new Integer(this.order).compareTo(new Integer(o.order));
+	}
 }
--- api/src/main/java/org/openmrs/module/appframework/domain/Extension.java
+++ api/src/main/java/org/openmrs/module/appframework/domain/Extension.java
@@ -1,119 +1,121 @@
 package org.openmrs.module.appframework.domain;
 
+import java.util.Map;
+
 import org.codehaus.jackson.annotate.JsonProperty;
 import org.hibernate.validator.constraints.NotEmpty;
 import org.openmrs.module.appframework.domain.validators.ValidationErrorMessages;
 
-import java.util.Map;
-
 public class Extension implements Comparable<Extension> {
-
-    @NotEmpty(message = ValidationErrorMessages.EXTENSION_ID_NOT_EMPTY_MESSAGE)
-    @JsonProperty
-    protected String id;
-
-    @JsonProperty
-    protected String appId;
-
-    @JsonProperty
-    protected String extensionPointId;
-
-    @JsonProperty
-    protected String type;
-
-    @JsonProperty
-    protected String label;
-
-    @JsonProperty
-    protected String url;
-
-    @JsonProperty
-    protected int order;
-
-    @JsonProperty
-    protected Map<String, Object> extensionParams;
-
-
-    public Extension() {
-    }
-
-    public Extension(String id, String appId, String extensionPointId, String type, String label, String url, int order) {
-        this.id = id;
-        this.appId = appId;
-        this.extensionPointId = extensionPointId;
-        this.type = type;
-        this.label = label;
-        this.url = url;
-        this.order = order;
-    }
-
-    public Extension(String id, String appId, String extensionPointId, String type, String label, String url, int order, Map<String, Object> extensionParams) {
-        this.id = id;
-        this.appId = appId;
-        this.extensionPointId = extensionPointId;
-        this.type = type;
-        this.label = label;
-        this.url = url;
-        this.order = order;
-        this.extensionParams = extensionParams;
-    }
-
-
-    public String getId() {
-        return id;
-    }
-
-    public String getAppId() {
-        return appId;
-    }
-
-    public String getExtensionPointId() {
-        return extensionPointId;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public String getLabel() {
-        return label;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public int getOrder() {
-        return order;
-    }
-
-    public Map<String, Object> getExtensionParams() {
-        return extensionParams;
-    }
-
-    public void setExtensionParams(Map<String, Object> extensionParams) {
-        this.extensionParams = extensionParams;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Extension)) return false;
-
-        Extension extension = (Extension) o;
-
-        if (id != null ? !id.equals(extension.id) : extension.id != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        return id != null ? id.hashCode() : 0;
-    }
-
-    @Override
-    public int compareTo(Extension o) {
-        return new Integer(this.order).compareTo(o.order);
-    }
+	
+	@NotEmpty(message = ValidationErrorMessages.EXTENSION_ID_NOT_EMPTY_MESSAGE)
+	@JsonProperty
+	protected String id;
+	
+	@JsonProperty
+	protected String appId;
+	
+	@JsonProperty
+	protected String extensionPointId;
+	
+	@JsonProperty
+	protected String type;
+	
+	@JsonProperty
+	protected String label;
+	
+	@JsonProperty
+	protected String url;
+	
+	@JsonProperty
+	protected int order;
+	
+	@JsonProperty
+	protected Map<String, Object> extensionParams;
+	
+	public Extension() {
+	}
+	
+	public Extension(String id, String appId, String extensionPointId, String type, String label, String url, int order) {
+		this.id = id;
+		this.appId = appId;
+		this.extensionPointId = extensionPointId;
+		this.type = type;
+		this.label = label;
+		this.url = url;
+		this.order = order;
+	}
+	
+	public Extension(String id, String appId, String extensionPointId, String type, String label, String url, int order,
+	    Map<String, Object> extensionParams) {
+		this.id = id;
+		this.appId = appId;
+		this.extensionPointId = extensionPointId;
+		this.type = type;
+		this.label = label;
+		this.url = url;
+		this.order = order;
+		this.extensionParams = extensionParams;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public String getAppId() {
+		return appId;
+	}
+	
+	public String getExtensionPointId() {
+		return extensionPointId;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+	public String getLabel() {
+		return label;
+	}
+	
+	public String getUrl() {
+		return url;
+	}
+	
+	public int getOrder() {
+		return order;
+	}
+	
+	public Map<String, Object> getExtensionParams() {
+		return extensionParams;
+	}
+	
+	public void setExtensionParams(Map<String, Object> extensionParams) {
+		this.extensionParams = extensionParams;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (!(o instanceof Extension))
+			return false;
+		
+		Extension extension = (Extension) o;
+		
+		if (id != null ? !id.equals(extension.id) : extension.id != null)
+			return false;
+		
+		return true;
+	}
+	
+	@Override
+	public int hashCode() {
+		return id != null ? id.hashCode() : 0;
+	}
+	
+	@Override
+	public int compareTo(Extension o) {
+		return new Integer(this.order).compareTo(o.order);
+	}
 }
