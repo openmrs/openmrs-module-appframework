9d3f4646a4e11afcd1df948cbbcef7108b869415
djazayeri <jazayeri@alum.mit.edu>
2013-06-07 18:17:01 -0700

    AF-24 - Allow other modules to use appframework-api's test-jar without getting its test apps

--- api/src/test/java/org/openmrs/module/appframework/AppTestUtil.java
+++ api/src/test/java/org/openmrs/module/appframework/AppTestUtil.java
@@ -1,6 +1,7 @@
 package org.openmrs.module.appframework;
 
 import org.openmrs.module.appframework.domain.AppDescriptor;
+import org.openmrs.module.appframework.domain.AppTemplate;
 import org.openmrs.module.appframework.factory.AppConfigurationLoaderFactory;
 import org.openmrs.module.appframework.factory.AppFrameworkFactory;
 import org.openmrs.module.appframework.repository.AllAppDescriptors;
@@ -33,6 +34,20 @@ public class AppTestUtil {
         AllAppDescriptors allAppDescriptors = new AllAppDescriptors(validator);
         new AppFrameworkActivator().registerAppsAndExtensions(Arrays.<AppFrameworkFactory>asList(new AppConfigurationLoaderFactory()), new AllAppTemplates(validator), allAppDescriptors, new AllExtensions(validator));
         return allAppDescriptors.getAppDescriptor(id);
+    }
+
+    /**
+     * For mock-based tests.
+     * Parses AppTemplates, Apps, and Extensions, and returns a specific template.
+     * @param id
+     * @return
+     */
+    public static AppTemplate getAppTemplate(String id) {
+        Validator validator = mock(Validator.class);
+        when(validator.validate(anyObject())).thenReturn(Collections.<ConstraintViolation<Object>>emptySet());
+        AllAppTemplates allAppTemplates = new AllAppTemplates(validator);
+        new AppFrameworkActivator().registerAppsAndExtensions(Arrays.<AppFrameworkFactory>asList(new AppConfigurationLoaderFactory()), allAppTemplates, new AllAppDescriptors(validator), new AllExtensions(validator));
+        return allAppTemplates.getAppTemplate(id);
     }
 
 }
---
+++ api/src/test/resources/apps/README.txt
@@ -0,0 +1,2 @@
+In order to allow other modules to include appframework-api's test-jar, any files that you'd want to put in this folder
+to test appframework-api should go in the appsfortesting module.
