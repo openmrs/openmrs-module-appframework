5990207a9c7b3110fa9b97b38df1a048c0c95764
jdegraft <jdegraft_johnson@msn.com>
2016-01-06 06:47:50 -0600

    RA-968 Built module for OpenMRS 2.0 platform

--- api/src/test/java/org/openmrs/module/appframework/factory/AppConfigurationLoaderFactoryTest.java
+++ api/src/test/java/org/openmrs/module/appframework/factory/AppConfigurationLoaderFactoryTest.java
@@ -7,7 +7,6 @@ import org.openmrs.module.appframework.repository.AllAppDescriptors;
 import org.openmrs.module.appframework.repository.AllFreeStandingExtensions;
 import org.openmrs.test.BaseModuleContextSensitiveTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.annotation.DirtiesContext;
 
 import java.io.IOException;
 import java.util.List;
@@ -26,7 +25,6 @@ public class AppConfigurationLoaderFactoryTest extends BaseModuleContextSensitiv
     AllFreeStandingExtensions allFreeStandingExtensions;
 
     @Test
-    @DirtiesContext
     public void testConfigurationLoad() throws IOException {
 
         List<AppDescriptor> appDescriptors = appConfigurationLoaderFactory.getAppDescriptors();
